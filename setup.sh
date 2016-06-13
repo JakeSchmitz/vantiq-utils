8649179fec302f8de55efee9907b7e805d17dcca
Jacob Schmitz <jake.schmitz101@gmail.com>
2016-06-13 13:47:12 -0700

    setup script is testable

--- setup.sh
+++ setup.sh
@@ -5,6 +5,11 @@
 # This script should install all of the dependencies of the Vantiq Server necessary for running on 
 # an ARM device, as well as install the vantiq server itself
 
+USRNAME="$1"
+PASSWD="$2"
+
+echo "Using the following credentials to clone ag2rs: $USRNAME $PASSWD\n"
+
 # Raspbian currently ships with Oracle JDK 1.8, so we should be good on the Java front
 # We will need to install and setup h2
 # we're going to install h2 into /usr/share
@@ -22,9 +27,10 @@ unzip h2gis.zip
 java -jar h2gis-standalone/h2-dist-1.2.3.jar &  
 
 # Get the Vantiq server
-git clone https://github.com/Vantiq/ag2rs.git
+git clone https://$USRNAME:$PASSWD@github.com/Vantiq/ag2rs.git
 cd ag2rs
 ./gradlew clean build -x test
+echo "Vantiq server should now be installed in /usr/share/ag2rs"
 
 popd # /usr/share
 
