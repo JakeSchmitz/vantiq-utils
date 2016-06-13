3221d20d521edf1c213991dc79e7b803a2668a28
Jacob Schmitz <jake.schmitz101@gmail.com>
2016-06-13 14:26:11 -0700

    get geojson-h2 branch of ag2rs

--- setup.sh
+++ setup.sh
@@ -10,6 +10,14 @@ PASSWD="$2"
 
 echo "Using the following credentials to clone ag2rs: $USRNAME $PASSWD\n"
 
+# set up database data directory
+pushd /
+
+mkdir -p data
+chmod 755 data
+
+popd # /
+
 # Raspbian currently ships with Oracle JDK 1.8, so we should be good on the Java front
 # We will need to install and setup h2
 # we're going to install h2 into /usr/share
@@ -23,14 +31,18 @@ pushd /usr/share
 # This should include H2 itself
 wget https://github.com/orbisgis/h2gis/releases/download/v1.2.3/h2-dist-1.2.3-bin.zip -O h2gis.zip
 unzip h2gis.zip
-# Start H2 now
-java -jar h2gis-standalone/h2-dist-1.2.3.jar &  
 
 # Get the Vantiq server
 git clone https://$USRNAME:$PASSWD@github.com/Vantiq/ag2rs.git
 cd ag2rs
+git fetch
+git checkout jbs-geojson-h2
 ./gradlew clean build -x test
 echo "Vantiq server should now be installed in /usr/share/ag2rs"
 
+# Start H2 now
+java -jar h2gis-standalone/h2-dist-1.2.3.jar &  
+
 popd # /usr/share
+
 
